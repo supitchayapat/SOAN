1673ac11354032f7e237b0bee66957bc8653f9c8
Issam wakidi <issam.wakidi@gmail.com>
2016-03-24 16:14:49 +0100

    adding todo to improve a condition check

--- utils.js
+++ utils.js
@@ -7,6 +7,8 @@ function formatPhoneNumber10DigitWithSpageFR(txt, backSpacePressed){
         var newString = ""
         for(var i = 0; i < txt.length; i++){
             if(txt[i] !== " ") newString = newString + txt[i]
+            /* TODO : replacing this condition with newString.length % 3 === 2 leads to a bug,
+            please retry later if the bug is still there investigate and declare it*/
             if((newString.length  === 2 ||newString.length  === 5 ||
                 newString.length  === 8 ||newString.length  === 11)){
                  newString = newString + " "
