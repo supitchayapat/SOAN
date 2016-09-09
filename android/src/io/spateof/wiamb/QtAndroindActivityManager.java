699c5b8d82fc33219d80e403e577d7267789d96e
Ali Diouri <alidiouri@gmail.com>
2016-09-09 19:46:09 +0100

    removing extra imports

--- android/src/io/spateof/wiamb/QtAndroindActivityManager.java
+++ android/src/io/spateof/wiamb/QtAndroindActivityManager.java
@@ -1,9 +1,7 @@
 package io.spateof.wiamb;
 
 import android.app.Activity;
-import android.view.KeyEvent;
 import org.qtproject.qt5.android.bindings.QtActivity;
-import org.qtproject.qt5.android.bindings.QtApplication;
 
 public class QtAndroindActivityManager extends QtActivity
 {
